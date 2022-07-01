26ac28eb6028da181ee64d852a40b0e249df4f78
Avi Vahl <avi.vahl@wix.com>
2022-07-01 15:29:15 +0300

    refactor: move global type augmentation to bottom every lit comp would need this, and its repetitive

--- src/simple-greeting.ts
+++ src/simple-greeting.ts
@@ -1,12 +1,6 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
-declare global {
-  interface HTMLElementTagNameMap {
-    "simple-greeting": SimpleGreeting;
-  }
-}
-
 @customElement("simple-greeting")
 export class SimpleGreeting extends LitElement {
   static override styles = css`
@@ -20,5 +14,11 @@ export class SimpleGreeting extends LitElement {
 
   protected override render() {
     return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "simple-greeting": SimpleGreeting;
   }
 }
